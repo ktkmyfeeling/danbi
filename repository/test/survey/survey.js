838fdfede1bdca5f41a252e4d1290601370ce62b
ktkmyfeeling <ktkmyfeeling@nate.com>
2016-10-31 22:17:49 +0900

    20161031 화요일

--- repository/test/survey/survey.js
+++ repository/test/survey/survey.js
@@ -1,19 +1,20 @@
 function add_question(){
 	var text = "";
+	text += "<tr>	<td>";
 	text += "<div>";
 	text += "<table class='question'>";
 	text += "<tr>";
-	text += "<td><input type='button' value='up'></td>";
-	text += "<td><input type='text' value='객관식 제목1'></td>";
+	text += "<td><input type='button' value='up' onclick='moveUp(this)'></td>";
+	text += "<td><input class='question_title' type='text' value='객관식 제목1'></td>";
 	text += "<td>";
-	text += "<select onchange='select_option(this)'>";
+	text += "<select class='question_type' onchange='select_option(this)'>";
 	text += "<option value='1' selected>객관식</option>";
 	text += "<option value='2'>주관식</option>";
 	text += "</select>";
 	text += "</td>";
 	text += "</tr>";
 	text += "<tr class='change_option'>";
-	text += "<td><input type='button' value='do'></td>";
+	text += "<td><input type='button' value='do' onclick='moveDown(this)'></td>";
 	text += "<td colspan='2'>";
 	text += "<table class='mutil_option'>";
 	text += "<tr>";
@@ -33,13 +34,18 @@ function add_question(){
 	text += "</tr>";
 	text += "</table>";
 	text += "</div>";
+	text += "</td></tr>";
 	
 	$(".survey_body").append(text);
 }
 
 function delete_question(that){
 //	console.log($(that).parent().parent().parent().parent().parent().html());
-	$(that).parent().parent().parent().parent().parent().remove();
+	if($('.question').length < 2){
+		alert('최소 한개 이상의 질문은 있어야 합니다. ');
+	}else{
+		$(that).parent().parent().parent().parent().parent().remove();
+	}
 //	console.log($(that).parent().parent().parent().parent().parent().parent().html());
 }
 
@@ -94,6 +100,51 @@ function add_option(that){
 function delete_option(that){
 	$(that).parent().parent().remove();
 }
+
+function moveUp(el){
+	var $tr = $(el).parent().parent().parent().parent().parent().parent().parent(); // 클릭한 버튼이 속한 tr 요소
+//	var tr = $(el).parent().parent();
+//	console.log(tr.html());
+	$tr.prev().before($tr); // 현재 tr 의 이전 tr 앞에 선택한 tr 넣기
+}
+
+function moveDown(el){
+	var $tr = $(el).parent().parent().parent().parent().parent().parent().parent(); // 클릭한 버튼이 속한 tr 요소
+	$tr.next().after($tr); // 현재 tr 의 다음 tr 뒤에 선택한 tr 넣기
+}
+
+function saveSurvey(){
+	var survey_object = new Object();
+	
+	var id = $("#title").val();
+	var study_id = $("#title").val();
+	var title = $("#title").val();
+	var sub_title = $("#sub_title").val();
+	
+	survey_object.id 						= id;
+	survey_object.study_id 		= study_id;
+	survey_object.title 				= title;
+	survey_object.sub_title 	= sub_title;
+	
+	$(".question").each(function(index){
+		var question_title = $(this).find('.question_title').val();
+		var question_type = $(this).find('.question_type').val();
+		var option_arr = new Array();
+		var option_text = $(this).find('.change_option input:text');
+
+		$(option_text).each(function(seq){
+			option_arr.push($(this).val());
+		});
+		
+		console.log(question_title);
+		console.log(question_type);
+		console.log(option_arr);
+	});
+	
+}
+
+
+
 
 
 
--- repository/test/survey/survey.php
+++ repository/test/survey/survey.php
@@ -1,7 +1,9 @@
 <html>
 
 <head>
-
+<?
+$study_id = date("YmdHis",time());
+?>
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script type='text/javascript' src='survey.js'></script>
 
@@ -16,78 +18,71 @@ div{
 
 <body style='margin: 0px;'>
 
-<div style='width: 1100px;'>
+<div style='width: 500px;'>
+	개설자 : <input id="id" type="text" value="test">
+	스터디 id : <input id="study_id" type="text" value="<?=$study_id ?>">
 	<!-- 제목 start -->
 	<div>
 		<table style='width: 100%; height: 100px;'>
 			<tr>
-				<td><input type='text' value='제목1'></td>
+				<td><input id="title" type='text' value='제목1'></td>
 			</tr>
 			<tr>
-				<td><input type='text' value='서브 제목1'></td>
+				<td><input id="sub_title" type='text' value='서브 제목1'></td>
 			</tr>
 		</table>
 	</div>
 	<!-- 제목 end -->
 	질문 시작
-	<div class='survey_body'>
-		<div>
-			<table class='question'>
-				<tr>
-					<td><input type='button' value='up'></td>
-					<td><input type='text' value='객관식 제목1'></td>
-					<td>
-						<select onchange='select_option(this)'>
-							<option value="1" selected>객관식</option>
-							<option value="2">주관식</option>
-						</select>
-					</td>
-				</tr>
-				<tr class='change_option'>
-					<td><input type='button' value='do'></td>
-					<td colspan='2'>
-						<table class='mutil_option'>
+	<div>
+		<table class='survey_body'>
+			<tr>
+				<td>
+					<div>
+						<table class='question'>
 							<tr>
-								<td><input type='text' value='객관식 옵션1'></td>
+								<td><input type='button' value='up' onclick='moveUp(this)'></td>
+								<td><input class='question_title' type='text' value='객관식 제목1'></td>
+								<td>
+									<select class='question_type' onchange='select_option(this)'>
+										<option value="1" selected>객관식</option>
+										<option value="2">주관식</option>
+									</select>
+								</td>
+							</tr>
+							<tr class='change_option'>
+								<td><input type='button' value='do' onclick='moveDown(this)'></td>
+								<td colspan='2'>
+									<table class='mutil_option'>
+										<tr>
+											<td><input type='text' value='객관식 옵션1'></td>
+											<td></td>
+										</tr>
+									</table>
+									<table>
+										<tr>
+											<td><input type='button' value='옵션추가' onclick='add_option(this)'></td>
+											<td></td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
 								<td></td>
+								<td></td>
+								<td><input type='button' value='질문삭제' onclick='delete_question(this)'></td>
 							</tr>
 						</table>
-						<table>
-							<tr>
-								<td><input type='button' value='옵션추가' onclick='add_option(this)'></td>
-								<td></td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-			</table>
-		</div>
+					</div>
+				</td>
+			</tr>
+		</table>
 	</div>
 	
 	<input type='button' value='질문 추가' onclick='add_question()'>
 	
-	
-	<table>
-		<tr>
-			<td><input type='button' value='up'></td>
-			<td><input type='text' value='주관식 제목1'></td>
-			<td>
-				<select>
-					<option selected>객관식</option>
-					<option>주관식</option>
-				</select>
-			</td>
-		</tr>
-		<tr>
-			<td><input type='button' value='do'></td>
-			<td>
-				<input type='text' value='주관식 옵션1' readonly>
-			</td>
-			<td></td>
-		</tr>
-	</table>
 </div>
-
+<input type="button" value="asdf" onclick="saveSurvey()">
 </body>
 
 
